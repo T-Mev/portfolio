170a70adb1882d719c4c41353820cef51a9d5b11
T-Mev <teddirect@gmail.com>
2021-03-28 22:12:34 -0400

    Initial work on about component

--- project/src/app/components/about/about.component.ts
+++ project/src/app/components/about/about.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  skills: string[] = [
+    "Java",
+    "Spring Boot",
+    "Hibernate",
+    "Gradle",
+    "Angular 8",
+    "JavaScript",
+    "TypeScript",
+    "Node.Js",
+    "HTML5",
+    "CSS3",
+    "Angular Material",
+    "Bootstrap 4",
+    "MySQL",
+    "NoSQL",
+    "Git",
+    "GitHub",
+    "RESTful APIs",
+    "Test-Driven Development",
+    "Agile / Scrum"
+  ];
+
   constructor() { }
 
   ngOnInit() {
